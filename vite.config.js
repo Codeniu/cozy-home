75e5e835a9f5bd1eedcb09abd04abd00995f7a20
codeniu <codeniu@163.com>
2026-09-15 16:41:28 +0800

    docs: 格式化 index.html 并将资源路径改为 cozy-home

--- vite.config.js
+++ vite.config.js
@@ -1,2 +1,4 @@
-import {defineConfig} from 'vite';
-export default defineConfig({build:{rollupOptions:{output:{manualChunks:{three:['three']}}}}});
+import { defineConfig } from 'vite'
+export default defineConfig({
+  build: { rollupOptions: { output: { manualChunks: { three: ['three'] } } } },
+})
